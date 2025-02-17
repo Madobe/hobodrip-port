097d7709eff687d7313a97bb33a77d782af6bfce
Madobe Nanami <sin_emerald@hotmail.com>
2025-02-17 02:31:50 -0700

    Fix lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
-import DailyChecklist from "@/views/DailyChecklist.vue"
-import TeamBuilder from "@/views/TeamBuilder.vue"
-import GiftBoxes from "@/views/GiftBoxes.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,17 +12,17 @@ const router = createRouter({
         {
             path: "/daily-checklist",
             name: "daily-checklist",
-            component: () => DailyChecklist,
+            component: () => import("@/views/DailyChecklist.vue"),
         },
         {
             path: "/team-builder",
             name: "team-builder",
-            component: () => TeamBuilder,
+            component: () => import("@/views/TeamBuilder.vue"),
         },
         {
             path: "/gift-boxes",
             name: "gift-boxes",
-            component: () => GiftBoxes,
+            component: () => import("@/views/GiftBoxes.vue"),
         },
     ],
 })
